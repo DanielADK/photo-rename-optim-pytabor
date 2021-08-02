03d5820f4293565278789c9e10129f8652bf1816
Daniel Adámek <adamedan@fit.cvut.cz>
2021-08-02 19:27:49 +0200

    tridic-dnu edited

--- tridicdnu.py
+++ tridicdnu.py
@@ -5,7 +5,3 @@ for name in slozky:
     if not os.path.exists(dirName):
         os.mkdir(dirName)
         print("Vytvářím: " , dirName)
-    dirName = ""+str(name)+".den-nej"
-    if not os.path.exists(dirName):
-        os.mkdir(dirName)
-        print("Vytvářím: " , dirName)
